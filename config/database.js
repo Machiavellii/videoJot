8cd7df0903c890148dbab527c43e916975b5c055
Machiavellii <33911723+Machiavellii@users.noreply.github.com>
2018-09-02 18:41:27 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if ((process.env.NODE_ENV === 'production')) {
   module.exports = {
-    mongoURI: 'mongodb://vjeko:serval85@ds139722.mlab.com:39722/vidjot-prod'
+    mongoURI: 'mongodb://*****:******@ds139722.mlab.com:39722/vidjot-prod'
   };
 } else {
   module.exports = {
